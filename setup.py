f65c4efabf00889e99520f0f8240d8548a4d25c0
Radziko <s204162@dtu.dk>
2023-01-04 16:25:29 +0100

    First commit, set up cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps project',
+    author='CarlJonasJoachimJulius',
+    license='MIT',
+)
