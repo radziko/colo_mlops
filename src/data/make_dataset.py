bc9dd337f964efafb03e379459e5d5e1489d8e37
CarlAnSc <carl.gyvelvej@gmail.com>
2023-01-05 16:51:59 +0100

    First dataprocesser

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,25 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import pickle
+import os
+import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import TensorDataset
+
+
+def unpickle(file):
+    with open(file, 'rb') as file:
+        dict = pickle.load(file, encoding='latin1')
+    return dict
+
+def transformer(data):
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0,), (1,)),
+                                    transforms.Lambda(lambda x: torch.flatten(torch.swapdims(x, 0, 1), start_dim=1)), # Remove flatten to make conv
+                                    transforms.Lambda(lambda x: x.to(torch.float32))])
+    return transform(data)
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -13,7 +32,31 @@ def main(input_filepath, output_filepath):
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    #Getting training data
+    train_data = []
+    train_labels = []
+    logger.info('Starting loading. Treating all files starting with \"data\" as training data.')
+    for filename in os.listdir(input_filepath):
+        if filename[:4] == 'data':
+            train_data.append(unpickle(f'{input_filepath}/{filename}')['data'])
+            train_labels.append(unpickle(f'{input_filepath}/{filename}')['labels'])
+    train_data, train_labels = np.concatenate(train_data), np.concatenate(train_labels)
+
+    #Getting test data
+    test_data = unpickle(f'{input_filepath}/test_batch')['data']
+    test_labels = unpickle(f'{input_filepath}/test_batch')['labels']
+
+    # Transforming data
+    train_set = {"data": transformer(train_data), "labels": torch.LongTensor(train_labels)}
+    train_set = TensorDataset(train_set['data'], train_set['labels']) 
+    test_set = {"data": transformer(test_data), "labels": torch.LongTensor(test_labels)}
+    test_set = TensorDataset(test_set['data'], test_set['labels']) 
+
+    torch.save(train_set, f=f'{output_filepath}/train.data')
+    torch.save(test_set, f=f'{output_filepath}/test.data')
+
+    logger.info('Finished. Raw data has been processed.')
+
 
 
 if __name__ == '__main__':
