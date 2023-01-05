845ecfb826c7c6af0618292417ba5acbe4b5e118
Jonas Hoffmann <jonas@triweb.dk>
2023-01-05 16:28:39 +0100

    rewrite make data to only use downloaded data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
+import pickle
 from pathlib import Path
 
 import click
+import numpy as np
+import torch
 from dotenv import find_dotenv, load_dotenv
+from sklearn.model_selection import train_test_split
+from torch import Generator
+from torch.utils.data import random_split
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
-from torch.utils.data import random_split
-from torch import Generator
-import torch
 
 
 @click.command()
@@ -23,28 +26,60 @@ def main(input_filepath, output_filepath, seed):
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
-    transform = transforms.Compose([transforms.ToTensor()])
+    # Load training data
+    training_datasets = []
 
-    training_data = CIFAR10(
-        root=os.path.join(input_filepath, "CIFAR10"),
-        train=True,
-        download=True,
-        transform=transform,
-    )
-    test_data = CIFAR10(
-        root=os.path.join(input_filepath, "CIFAR10"),
-        train=False,
-        download=True,
-        transform=transform,
-    )
-    # Create validation set from training data
-    training_data, validation_data = random_split(training_data, [0.95, 0.05], generator=Generator().manual_seed(seed))
+    for i in range(1, 6):
+        data_path = os.path.join(input_filepath, "CIFAR10", f"data_batch_{i}")
 
-    output_dir = os.path.join(output_filepath, "CIFAR10",)
+        with open(data_path, "rb") as f:
+            training_datasets.append(pickle.load(f, encoding="bytes"))
+
+    training_data = np.concatenate([ds[b"data"] for ds in training_datasets])
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Lambda(
+                lambda x: x.view(-1, 3, 32, 32)
+            ),  # Convert it to square images
+            transforms.Normalize((0,), (1,)),  # Then standardize
+        ]
+    )
+
+    training_images = transform(training_data)
+    training_labels = torch.LongTensor(
+        np.concatenate([ds[b"labels"] for ds in training_datasets])
+    )
+
+    # Split training set into training and validation
+    (
+        training_images,
+        validation_images,
+        training_labels,
+        validation_labels,
+    ) = train_test_split(
+        training_images, training_labels, test_size=0.05, random_state=seed
+    )
+
+    # Load test data
+    with open(os.path.join(input_filepath, "CIFAR10", "test_batch"), "rb") as f:
+        test_dataset = pickle.load(f, encoding="bytes")
+
+    test_images = transform(test_dataset[b"data"])
+    test_labels = torch.LongTensor(test_dataset[b"labels"])
+
+    train_data = {"images": training_images, "labels": training_labels}
+    validation_data = {"images": validation_images, "labels": validation_labels}
+    test_data = {"images": test_images, "labels": test_labels}
+
+    output_dir = os.path.join(output_filepath, "CIFAR10")
     os.makedirs(output_dir, exist_ok=True)
 
-    torch.save(training_data, os.path.join(output_dir, "train.pt"))
-    torch.save(validation_data, os.path.join(output_filepath, "CIFAR10", "validation.pt"))
+    torch.save(train_data, os.path.join(output_dir, "train.pt"))
+    torch.save(
+        validation_data, os.path.join(output_filepath, "CIFAR10", "validation.pt")
+    )
     torch.save(test_data, os.path.join(output_filepath, "CIFAR10", "test.pt"))
 
 
