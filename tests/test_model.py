29978ac6103db5762fe01a60b790ba1be850704d
radziko <s204162@dtu.dk>
2023-01-11 12:40:13 +0100

    added test workflow

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,10 @@
 import torch
 
-from src.models.model import CIFAR10Model, get_model
+from src.models.model import CIFAR10Module, get_model
 
 
 def test_model_output():
-    model = CIFAR10Model(classifier=get_model("resnet18", False))
+    model = CIFAR10Module(classifier=get_model("resnet18", False))
 
     batchsize = 64
     x = torch.rand((batchsize, 3, 32, 32))
