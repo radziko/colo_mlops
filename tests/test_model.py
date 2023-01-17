8002e54998406161a474f4690d965b230d24c17f
radziko <s204162@dtu.dk>
2023-01-17 14:01:50 +0100

    fixed tests again

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@ import torch
 
 from src.models.model import CIFAR10Module, create_model
 
-
+@torch.no_grad()
 def test_model_output():
     model = CIFAR10Module(classifier=create_model())
 
@@ -10,8 +10,10 @@ def test_model_output():
     x = torch.rand((batchsize, 3, 32, 32))
     out = model.forward(x)
 
+    print(torch.sum(torch.exp(out)), torch.tensor(batchsize, dtype=torch.float))
+
     assert out.shape == torch.Size([batchsize, 10])
-    assert torch.sum(torch.exp(out)) == torch.tensor(batchsize)
+    assert torch.sum(torch.exp(out)) == (torch.tensor(batchsize, dtype=torch.float))
 
 
 if __name__ == "__main__":
